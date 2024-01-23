c94b541d53d44fb51ae7299a8d180897ce8bce39
Florian Lepage <florian.kyn@gmail.com>
2024-01-24 00:25:22 +0100

    Update background.js

--- public/background.js
+++ public/background.js
@@ -1,4 +1,17 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+
+let isExtensionEnabled = false;
+
+chrome.storage.local.get(['isEnabled'], function(result) {
+    isExtensionEnabled = result.isEnabled ?? false;
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    if (changes.isEnabled) {
+        isExtensionEnabled = changes.isEnabled.newValue;
+    }
+});
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(async function (details) {
   const bannedUrls = [
     'youtube.com/shorts/',
     'instagram.com/reels/',
@@ -7,12 +20,18 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     "tiktok.com/en/",
   ];
 
-  if (bannedUrls.some(url => details.url.includes(url))) {
+  let isAllowerd = await chrome.storage.local.get("isEnabled");
+
+  if(isAllowerd.isEnabled) {
+    if (bannedUrls.some(url => details.url.includes(url))) {
       chrome.scripting.executeScript({
           target: { tabId: details.tabId },
           function: blankOutPage
       });
   } 
+  }
+
+
 });
 
 function blankOutPage() {

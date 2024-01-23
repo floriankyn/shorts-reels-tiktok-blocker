969246dd49e1824d4e5c7a5db3a7ae0a3baa5549
Florian Lepage <florian.kyn@gmail.com>
2024-01-24 00:01:38 +0100

    Update background.js

--- public/background.js
+++ public/background.js
@@ -1,10 +1,18 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  if (details.url.includes('youtube.com/shorts/')) {
+  const bannedUrls = [
+    'youtube.com/shorts/',
+    'instagram.com/reels/',
+    "tiktok.com/",
+    "tiktok.com/@",
+    "tiktok.com/en/",
+  ];
+
+  if (bannedUrls.some(url => details.url.includes(url))) {
       chrome.scripting.executeScript({
           target: { tabId: details.tabId },
           function: blankOutPage
       });
-  }
+  } 
 });
 
 function blankOutPage() {

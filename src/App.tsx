a5415e7bc168fc4d6e827c925f7cc01bd63fc888
Florian Lepage <florian.kyn@gmail.com>
2024-01-23 23:23:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import Status from './components/Status.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOn, setIsOn] = useState(false)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className='flex items-center justify-center my-10'>
+        <button className='outline-0  bg-transparent' onClick={() => setIsOn(!isOn)}>
+          <img src="./stop.png" width={200} height={200} alt="" className=' hover:scale-110 duration-300' />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      </div>
+
+      <Status isOn={isOn} />
+
+      <div className='flex justify-center items-center gap-x-10'>
+        <h1 className='font-bold text-4xl'>
+          Shorts
+        </h1>
+        <h1 className='font-bold text-4xl'>
+          Reels
+        </h1>
+        <h1 className='font-bold text-4xl'>
+          TikTok
+        </h1>
+      </div>
+      <div className='my-5'>
+        <p className='text-lg font-medium'>
+          Get a rid of those life sucking apps and start living your life.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
